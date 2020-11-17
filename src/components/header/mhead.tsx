f1c6ff9c68e953c53b5370aea87527d54b5590b9
光啊啊 <641194367@qq.com>
2020-11-17 22:45:47 +0800

    add scss

--- src/components/header/mhead.tsx
+++ src/components/header/mhead.tsx
@@ -1,24 +1,30 @@
 
 import React from 'react';
 import banner from '../../static/banner.jpg'
-import './mhead.css'
+import './mhead.scss'
 function mheader(){
     return(
         <div  className="header-layout">
             <TabBar></TabBar>
-            <Banner></Banner>
+            {/* <Banner></Banner> */}
         </div>
     )
 }
 class TabBar extends React.Component<any,any>{
     constructor(props:any){
         super(props)
+        this.state={
+            arrList:[{title:'推荐音乐'},{title:'热歌榜'},{title:'搜索'}],
+            activeIndex:1
+        }
     }
     render(){
         let arrList = [{title:'推荐音乐'},{title:'热歌榜'},{title:'搜索'}]
         let arr:Array<any> = []
+        
         for(let i = 0;i<arrList.length;i++){
-            arr.push(<div className="item">{arrList[i].title}</div>)
+            let style = this.state.activeIndex == i?'item-active':''
+            arr.push(<div className="item item-acitve"><div className={style}></div>{arrList[i].title}</div>)
         }
         // arrList.forEach(element:<any,any> => {
          
