22363d8debf3139a267f5e043611dda6f9b5a106
光啊啊 <641194367@qq.com>
2020-11-17 23:18:02 +0800

    btn clikc

--- src/components/header/mhead.tsx
+++ src/components/header/mhead.tsx
@@ -15,27 +15,20 @@ class TabBar extends React.Component<any,any>{
         super(props)
         this.state={
             arrList:[{title:'推荐音乐'},{title:'热歌榜'},{title:'搜索'}],
-            activeIndex:1
+            activeIndex:0
         }
+    }
+    tabBar=(index:Number)=>{
+
     }
     render(){
         let arrList = [{title:'推荐音乐'},{title:'热歌榜'},{title:'搜索'}]
-<<<<<<< HEAD
-        let arr:Array<any> = []
-        
-        for(let i = 0;i<arrList.length;i++){
-            let style = this.state.activeIndex == i?'item-active':''
-            arr.push(<div className="item item-acitve"><div className={style}></div>{arrList[i].title}</div>)
-        }
-        // arrList.forEach(element:<any,any> => {
-         
-        // });
-=======
         let arr:Array<JSX.Element> = []
-        arrList.forEach(item => {
-         arr.push(<div className="item">{item.title}</div>)
+        
+        arrList.forEach((item,index) => {
+            let active = this.state.activeIndex == index?'item-active':''
+            arr.push(<div className="item"><div className={active}></div>{item.title}</div>)
         });
->>>>>>> 9a4b22c69f6ec166873dc8ebe5f8fa5c335671de
         return(
             <div className='tabbar'>
                 {arr}
