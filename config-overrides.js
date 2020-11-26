fb8735fd00101f9179c89597f17483163d1f9d5e
yellowwwwww <641194367@qq.com>
2020-11-26 11:20:53 +0800

    fix global sass

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,18 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports ,adjustStyleLoaders} = require('customize-cra');
 
 module.exports = override(
-       fixBabelImports('import', {
-         libraryName: 'antd-mobile',
-         style: 'css',
-       }),
+      //  fixBabelImports('import', {
+      //    libraryName: 'antd-mobile',
+      //    style: 'css',
+      //  }),
+       adjustStyleLoaders(rule => {
+        if (rule.test.toString().includes('scss')) {
+          rule.use.push({
+            loader: require.resolve('sass-resources-loader'),
+            options: {
+              resources: './src/global.scss'
+            }
+          });
+        }
+      })
      );
